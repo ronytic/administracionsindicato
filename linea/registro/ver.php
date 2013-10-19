018f52caf659adf177172eb600f9b5f6a7d3bfe9
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-10-18 20:08:25 -0400

    Agredado Registro de Linea

---
+++ linea/registro/ver.php
@@ -0,0 +1,34 @@
+<?php
+include_once("../../impresion/pdf.php");
+$titulo="Reporte de Sindicato";
+$id=$_GET['id'];
+class PDF extends PPDF{
+	
+}
+
+include_once("../../class/sindicato.php");
+$sindicato=new sindicato;
+$sin=array_shift($sindicato->mostrar($id));
+
+include_once("../../class/proveedor.php");
+$proveedor=new proveedor;
+$prov=array_shift($proveedor->mostrar($pro['codproveedor']));
+
+$pdf=new PDF("P","mm","letter");
+
+$pdf->AddPage();
+mostrarI(array("Nombre"=>$sin['nombre'],
+				"Personería Jurídica"=>$sin['personeriajuridica'],
+				"Nombre del Responsable"=>$sin['nombreresponsable'],
+				"Teléfono"=>$sin['telefono'],
+				"Dirección"=>$sin['direccion'],
+				"Observación"=>$sin['observacion'],
+			));
+
+/*$foto="../foto/".$emp['foto'];
+if(!empty($emp['foto']) && file_exists($foto)){
+	$pdf->Image($foto,140,50,40,40);	
+}
+*/
+$pdf->Output();
+?>

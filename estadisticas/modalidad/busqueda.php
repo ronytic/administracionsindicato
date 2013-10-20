689bcdaf10f6a8c92e82d6d54512a5c743695ab0
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-10-20 18:28:41 -0400

    Corregido Modalidad

--- estadisticas/modalidad/busqueda.php
+++ estadisticas/modalidad/busqueda.php
@@ -1,6 +1,6 @@
 <?php
 include_once("../../login/check.php");
-$titulo="Estadísticas de Lineas por Modalidades";
+$titulo="Estadísticas de Líneas por Modalidades";
 extract($_POST);
 
 $codsindicato=$codsindicato!=''?" codsindicato='$codsindicato'":'';

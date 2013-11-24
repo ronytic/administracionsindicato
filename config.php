c1ce4f16c2576cdc2b6d56155b97b5c45dc5f01b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2013-11-24 16:34:42 -0400

    Modificado Configuración

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$title="Sistema de Seguimiento y Control de Linea de Sindicatos";
+$title="";
 $siglas="SISCOS";
 $directory="administracionsindicato";//ej:carp/ sin el "/"
 $url="http://localhost/";//ej:http:"localhost/"
-$gestion="2013";
+$gestion="";
 $lema='"Gobierno Autonomo de la Ciudad de El Alto"';
 $anio="2013";
 
 $ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
-//$tp="D3W1N8E0E4B4B4";
+$tp="D3W1N8E0E4B4B4";
 ?>

26847689cad1f569e0e9e446dcd2bd25e098b7c0
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-01-14 21:13:07 -0400

    Corección de Reporte

--- linea/registro/reportegeneral.php
+++ linea/registro/reportegeneral.php
@@ -28,99 +28,164 @@ $pdf->CuadroCuerpoPersonalizado($pdf->w-40,mb_strtoupper($sin['nombre'],"utf8"),
 $pdf->CuadroCuerpoPersonalizado($pdf->w-40,"RECORRIDO AUTORIZADO",0,"C",0,"B");$pdf->Ln();
 $pdf->CuadroCuerpoPersonalizado($pdf->w-40,"COMISIÓN INTERINSTITUCIONAL \"EL ALTO\"",0,"C",0,"B");
 $pdf->Ln(10);
-$pdf->CuadroCuerpoPersonalizado(30,"Responsable",1,"",1,"B");
-$pdf->CuadroCuerpoPersonalizado(60,$sin['nombreresponsable'],0,"",1,"");
-$pdf->CuadroCuerpoPersonalizado(30,"C.I.",1,"",1,"B");
-$pdf->CuadroCuerpoPersonalizado(60,$sin['ciresponsable'],0,"",1,"");
-$pdf->Ln();
-$pdf->CuadroCuerpoPersonalizado(30,"Dirección",1,"",1,"B");
-$pdf->CuadroCuerpoPersonalizado(60,$sin['direccion'],0,"",1,"");
-$pdf->CuadroCuerpoPersonalizado(30,"Pers. Jurídica",1,"",1,"B");
-$pdf->CuadroCuerpoPersonalizado(60,$sin['personeriajuridica'],0,"",1,"");
-$pdf->Ln();
-$pdf->CuadroCuerpoPersonalizado(30,"Teléfono",1,"",1,"B");
-$pdf->CuadroCuerpoPersonalizado(60,$sin['telefono'],0,"",1,"");
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte3" or $mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(30,"Nº Pasajeros",1,"",1,"B");
-	$pdf->CuadroCuerpoPersonalizado(60,$lin['numeropasajeros'],0,"",1,"");
-}
-$pdf->Ln(10);
 
-$pdf->CuadroCuerpoPersonalizado(90,"MODALIDAD",1,"C",1,"B");
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte2" or $mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(90,"SERVICIO",1,"C",1,"B");
-}
-$pdf->Ln();
-$pdf->CuadroCuerpoPersonalizado(90,$mod['nombre'],0,"",1,"");
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte2" or $mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(90,$ser['nombre'],0,"",1,"");
-}
-$pdf->Ln();
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte4"){
+
+if($mod['tiporeporte']=="reporte1"){//Minibuses,microbus,carrys
+	$pdf->CuadroCuerpoPersonalizado(30,"Pers. Jurídica",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['personeriajuridica'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['nombreresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Dirección",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['direccion'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"C.I. Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['ciresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Teléfono",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['telefono'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Nº Pasajeros",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$lin['numeropasajeros'],0,"",1,"");
+	$pdf->Ln(10);
+	
+	$pdf->CuadroCuerpoPersonalizado(90,"MODALIDAD",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(92,"SERVICIO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,$mod['nombre'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(92,$ser['nombre'],0,"",1,"");
+	
+	$pdf->Ln();
 	$pdf->CuadroCuerpoPersonalizado(90,"PARADA INICIAL",1,"C",1,"B");
-	$pdf->CuadroCuerpoPersonalizado(90,"PARADA FINAL",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(92,"PARADA FINAL",1,"C",1,"B");
 	$pdf->Ln();
 	$pdf->CuadroCuerpoPersonalizado(90,$lin['paradainicial'],0,"",1,"");
-	$pdf->CuadroCuerpoPersonalizado(90,$lin['paradafinal'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(92,$lin['paradafinal'],0,"",1,"");
 	$pdf->Ln();
-}
-if($mod['tiporeporte']=="reporte2" or $mod['tiporeporte']=="reporte3"){
-	$pdf->CuadroCuerpoPersonalizado(90,"PARADA AUTORIZADA",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(182,"LONGITUD DE TRAMO",1,"C",1,"B");
 	$pdf->Ln();
-	$pdf->CuadroCuerpoPersonalizado(90,$lin['paradaautorizada'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(182,$lin['longitudtramo'],0,"",1,"");
 	$pdf->Ln();
-}
-$pdf->Ln();
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(180,"LONGITUD DE TRAMO",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(90,"TRAYECTO DE IDA",1,"C",1,"B");
+	$pdf->Line($pdf->GetX(),$pdf->GetY(),$pdf->GetX(),180);
+	$pdf->CuadroCuerpoPersonalizado(92,"TRAYECTO DE RETORNO",1,"C",1,"B");
 	$pdf->Ln();
-	$pdf->CuadroCuerpoPersonalizado(180,$lin['longitudtramo'],0,"",1,"");
-	$pdf->Ln();
-}
-$pdf->Ln(10);
-if($mod['tiporeporte']=="reporte1" or $mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(90,"IDA",1,"C",1,"B");
-	$pdf->CuadroCuerpoPersonalizado(90,"RETORNO",1,"C",1,"B");
-	$pdf->Ln();
+	
 	$y=$pdf->GetY();
-	$pdf->CuadroCuerpoMulti(90,$lin['trayectoida'],9,"",1);
+	$pdf->CuadroCuerpoMulti(90,$lin['trayectoida'],9,"",0);
 	$x=$pdf->GetY();
 	$pdf->SetXY(108,$y);
-	$pdf->CuadroCuerpoMulti(90,$lin['trayectovuelta'],9,"",1);
-}
-if($mod['tiporeporte']=="reporte2" or $mod['tiporeporte']=="reporte3"){
-	$pdf->CuadroCuerpoPersonalizado(90,"PARADA AUTORIZADA",1,"C",1,"B");
-	if($mod['tiporeporte']=="reporte2"){
-		$pdf->CuadroCuerpoPersonalizado(90,"VEHICULOS AUTORIZADOS",1,"C",1,"B");
-	}else{
-		$pdf->CuadroCuerpoPersonalizado(90,"CARACTERISTICAS DEL VEHICULO",1,"C",1,"B");	
-	}
+	$pdf->CuadroCuerpoMulti(92,$lin['trayectovuelta'],9,"",0);
 	$pdf->Ln();
-	$y=$pdf->GetY();
-	$pdf->CuadroCuerpoMulti(90,$lin['paradaautorizadadetallada'],9,"",1);
-	$x=$pdf->GetY();
-	$pdf->SetXY(108,$y);
-	if($mod['tiporeporte']=="reporte2"){
-		$pdf->CuadroCuerpoMulti(90,$lin['vehiculosautorizados'],9,"",1);
-	}else{
-		$pdf->CuadroCuerpoMulti(90,$lin['caracteristicasvehiculo'],9,"",1);
-	}
-}
-if($mod['tiporeporte']=="reporte4"){
-	$pdf->CuadroCuerpoPersonalizado(180,"CARACTERISTICAS DEL VEHICULO",1,"C",1,"B");	
+	$pdf->SetY(180);
+	$pdf->CuadroCuerpoPersonalizado(182,"OBSERVACIONES",1,"C",1,"B");
 	$pdf->Ln();
-	$pdf->CuadroCuerpoMulti(180,$lin['caracteristicasvehiculo'],9,"",1);
+	$pdf->CuadroCuerpoMulti(182,$lin['observacion'],0,"",0);
+	$pdf->ln();$pdf->ln();
 }
-$pdf->Ln(20);
-$pdf->CuadroCuerpoPersonalizado(180,"OBSERVACIONES",1,"C",1,"B");
-$pdf->Ln();
-$pdf->CuadroCuerpoMulti(180,$lin['observacion'],0,"",0);
-$pdf->ln();$pdf->ln();
 
-/*$foto="../foto/".$emp['foto'];
-if(!empty($emp['foto']) && file_exists($foto)){
-	$pdf->Image($foto,140,50,40,40);	
+if($mod['tiporeporte']=="reporte3" || $mod['tiporeporte']=="reporte2"){//TAXIS, RADIO TAXIS, TAXI FONOS Y CARGA
+	$pdf->CuadroCuerpoPersonalizado(30,"Pers. Jurídica",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['personeriajuridica'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['nombreresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Dirección",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['direccion'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"C.I. Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['ciresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Teléfono",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['telefono'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Nº Pasajeros",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$lin['numeropasajeros'],0,"",1,"");
+	$pdf->Ln(10);
+	
+	$pdf->CuadroCuerpoPersonalizado(90,"MODALIDAD",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(92,"SERVICIO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,$mod['nombre'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(92,$ser['nombre'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(182,"LONGITUD DE TRAMO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(182,$lin['longitudtramo'],0,"",1,"");
+	$pdf->Ln();
+	
+	$pdf->CuadroCuerpoPersonalizado(90,"PARADA AUTORIZADA",1,"C",1,"B");
+	$pdf->Line($pdf->GetX(),$pdf->GetY(),$pdf->GetX(),160);
+	$pdf->CuadroCuerpoPersonalizado(92,"VEHÍCHULOS AUTORIZADOS",1,"C",1,"B");
+	$pdf->Ln();
+	
+	$y=$pdf->GetY();
+	$pdf->CuadroCuerpoMulti(90,$lin['paradaautorizada'],9,"",0);
+	$x=$pdf->GetY();
+	$pdf->SetXY(108,$y);
+	$pdf->CuadroCuerpoMulti(92,$lin['vehiculosautorizados'],9,"",0);
+	$pdf->Ln();
+	$pdf->SetY(160);
+	
+	$pdf->CuadroCuerpoPersonalizado(182,"CARACTERÍSTICAS DEL VEHICULO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['caracteristicasvehiculo'],0,"",0);
+	$pdf->SetY(200);
+	$pdf->CuadroCuerpoPersonalizado(182,"OBSERVACIONES",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['observacion'],0,"",0);
 }
-*/
-$pdf->Output();
+
+if($mod['tiporeporte']=="reporte4"){//Trufi
+	$pdf->CuadroCuerpoPersonalizado(30,"Pers. Jurídica",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['personeriajuridica'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['nombreresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Dirección",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['direccion'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"C.I. Responsable",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$sin['ciresponsable'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(30,"Teléfono",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(60,$sin['telefono'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(30,"Nº Pasajeros",1,"",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(62,$lin['numeropasajeros'],0,"",1,"");
+	$pdf->Ln(10);
+	$pdf->CuadroCuerpoPersonalizado(90,"MODALIDAD",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(92,"SERVICIO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,$mod['nombre'],0,"",1,"");
+	$pdf->CuadroCuerpoPersonalizado(92,$ser['nombre'],0,"",1,"");
+	
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,"PARADA INICIAL",1,"C",1,"B");
+	$pdf->CuadroCuerpoPersonalizado(92,"PARADA FINAL",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,$lin['paradainicial'],0,"",1,"",8);
+	$pdf->CuadroCuerpoPersonalizado(92,$lin['paradafinal'],0,"",1,"",8);
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(182,"LONGITUD DE TRAMO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(182,$lin['longitudtramo'],0,"",1,"");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoPersonalizado(90,"TRAYECTO DE IDA",1,"C",1,"B");
+	$pdf->Line($pdf->GetX(),$pdf->GetY(),$pdf->GetX(),160);
+	$pdf->CuadroCuerpoPersonalizado(92,"TRAYECTO DE RETORNO",1,"C",1,"B");
+	$pdf->Ln();
+	
+	$y=$pdf->GetY();
+	$pdf->CuadroCuerpoMulti(90,$lin['trayectoida'],9,"",0);
+	$x=$pdf->GetY();
+	$pdf->SetXY(108,$y);
+	$pdf->CuadroCuerpoMulti(92,$lin['trayectovuelta'],9,"",0);
+	$pdf->Ln();
+	$pdf->SetY(160);
+	
+	$pdf->CuadroCuerpoPersonalizado(182,"CARACTERÍSTICAS DEL VEHICULO",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['caracteristicasvehiculo'],0,"",0);
+	$pdf->SetY(200);
+	$pdf->CuadroCuerpoPersonalizado(182,"OBSERVACIONES",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['observacion'],0,"",0);
+}
+
+$pdf->Output("reporte","I");
 ?>
--- linea/registro/ver.php
+++ linea/registro/ver.php
@@ -24,25 +24,53 @@ $ser=array_shift($servicio->mostrar($lin['codservicio']));
 $pdf=new PDF("P","mm","letter");
 
 $pdf->AddPage();
-mostrarI(array("Número de Linea"=>$lin['numerolinea'],
-				"Color"=>$lin['color'],
-				"Sindicato"=>$sin['nombre'],
-				"Parada Inicial"=>$lin['paradainicial'],
-				"Trayecto de Ida"=>""
-			));
-$pdf->CuadroCuerpoMulti(160,$lin['trayectoida']);
+if($mod['tiporeporte']=="reporte1" || $mod['tiporeporte']=="reporte4"){
+	mostrarI(array("Número de Linea"=>$lin['numerolinea'],
+					"Color"=>$lin['color'],
+					"Sindicato"=>$sin['nombre'],
+					"Parada Inicial"=>$lin['paradainicial'],
+					
+				));
+	$pdf->CuadroCuerpoPersonalizado(182,"Trayecto de Ida",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['trayectoida'],0,"",1);
+	
+	mostrarI(array("Parada Final"=>$lin['paradafinal'],
+					));
+	$pdf->CuadroCuerpoPersonalizado(182,"Trayecto de Vuelta",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['trayectovuelta'],0,"",1);
+	mostrarI(array(
+					"Modalidad"=>$mod['nombre'],
+					"Servicio"=>$ser['nombre']));
+	$pdf->CuadroCuerpoPersonalizado(182,"Observación",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['observacion'],0,"",0);
+}
 
-mostrarI(array("Parada Final"=>$lin['paradafinal'],
-				"Trayecto de Vuelta"=>""));
-$pdf->CuadroCuerpoMulti(160,$lin['trayectovuelta']);
-mostrarI(array(
-				"Modalidad"=>$mod['nombre'],
-				"Servicio"=>$ser['nombre'],
-				"Observación"=>$sin['observacion']));
+if($mod['tiporeporte']=="reporte3" || $mod['tiporeporte']=="reporte2"){
+	mostrarI(array("Número de Linea"=>$lin['numerolinea'],
+					"Sindicato"=>$sin['nombre'],
+					"Modalidad"=>$mod['nombre'],
+					"Parada Inicial"=>$lin['paradainicial'],
+					"Servicio"=>$ser['nombre']
+					
+				));
+	$pdf->CuadroCuerpoPersonalizado(182,"Parada Autorizada",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['paradainicial'],0,"",1);
+	
+	mostrarI(array("Vehiculos Autorizados"=>$lin['vehiculosautorizados'],
+					));
+	
+	$pdf->CuadroCuerpoPersonalizado(182,"Observaciones",1,"C",1,"B");
+	$pdf->Ln();
+	$pdf->CuadroCuerpoMulti(182,$lin['observacion'],0,"",0);
+}
 /*$foto="../foto/".$emp['foto'];
 if(!empty($emp['foto']) && file_exists($foto)){
 	$pdf->Image($foto,140,50,40,40);	
 }
 */
-$pdf->Output();
+$pdf->Output("reporte","I");
 ?>

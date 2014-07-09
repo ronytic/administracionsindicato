6ac82be490ef0bb9ae873883e592b7ca2c5a9e2f
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-07-09 17:58:09 -0400

    Corregido Reporte Tarjeta Operación

--- tip/to/ver.php
+++ tip/to/ver.php
@@ -25,79 +25,93 @@ $pdf=new FPDF("L","mm",array(217,168));
 $pdf->SetFont("arial","",10);
 $pdf->AddPage();
 
-$pdf->SetXY(20,48);
+$pdf->SetXY(9,47);
 $pdf->Cell(50,4,utf8_encode(mb_strtoupper($t['placa'],"utf8")),0,0,"C");
 
-$pdf->SetXY(35,63);
+$pdf->SetXY(35,61);
 $pdf->Cell(130,4,utf8_encode(mb_strtoupper($sin['nombre'],"utf8")),0,0,"C");
 
 $pdf->SetXY(160,84);
 $pdf->Cell(45,4,utf8_encode(mb_strtoupper($mod['nombre'],"utf8")),0,0,"C");
 
-foreach($lin as $l){
+$numeroslineas='';
+$numeroslineas2='';
+$i=0;
+foreach($lin as $l){$i++;
+	if($i<=13){
 	$numeroslineas.=$l['numerolinea']." - ";	
+	}else{
+	$numeroslineas2.=$l['numerolinea']." - ";	
+	}
 }
 
+$pdf->SetFont("arial","",9);
+
+$pdf->SetXY(25,72);
+//$pdf->Cell(130,4,utf8_encode(mb_strtoupper($numeroslineas,"utf8")),0,0,"C");
+
+$pdf->SetXY(25,70);
+$pdf->Cell(130,4,utf8_encode(mb_strtoupper($numeroslineas,"utf8")),0,0,"C");
+$pdf->SetXY(25,74);
+$pdf->Cell(130,4,utf8_encode(mb_strtoupper($numeroslineas,"utf8")),0,0,"C");
+
+$pdf->SetFont("arial","",10);
 $pdf->SetXY(30,84);
 $pdf->Cell(105,4,utf8_encode(mb_strtoupper($t['propetario'],"utf8")),0,0,"C");
 
-
-$pdf->SetXY(25,72);
-$pdf->Cell(130,4,utf8_encode(mb_strtoupper($numeroslineas,"utf8")),0,0,"C");
-
-$pdf->SetXY(50,90);
+$pdf->SetXY(50,92);
 $pdf->Cell(55,4,utf8_encode(mb_strtoupper($t['cipropetario'],"utf8")),0,0,"C");
 
-$pdf->SetXY(130,90);
+$pdf->SetXY(130,92);
 $pdf->Cell(70,4,utf8_encode(mb_strtoupper($t['polizaseguro'],"utf8")),0,0,"C");
 
-$pdf->SetXY(25,103);
+$pdf->SetXY(25,104);
 $pdf->Cell(50,4,utf8_encode(mb_strtoupper($t['marca'],"utf8")),0,0,"C");
 
-$pdf->SetXY(95,103);
+$pdf->SetXY(95,104);
 $pdf->Cell(50,4,utf8_encode(mb_strtoupper($t['modelo'],"utf8")),0,0,"C");
 
-$pdf->SetXY(155,103);
+$pdf->SetXY(155,104);
 $pdf->Cell(50,4,utf8_encode(mb_strtoupper($t['color'],"utf8")),0,0,"C");
 
-$pdf->SetXY(40,109);
+$pdf->SetXY(40,112);
 $pdf->Cell(65,4,utf8_encode(mb_strtoupper($t['clasevehiculo'],"utf8")),0,0,"C");
 
-$pdf->SetXY(130,109);
+$pdf->SetXY(130,112);
 $pdf->Cell(65,4,utf8_encode(mb_strtoupper($t['nasientos'],"utf8")),0,0,"C");
 
-$pdf->SetXY(40,116);
+$pdf->SetXY(40,119);
 $pdf->Cell(65,4,utf8_encode(mb_strtoupper($t['nmotor'],"utf8")),0,0,"C");
 
-$pdf->SetXY(130,116);
+$pdf->SetXY(130,119);
 $pdf->Cell(65,4,utf8_encode(mb_strtoupper($t['nchasis'],"utf8")),0,0,"C");
 
-$pdf->SetXY(122,123);
+$pdf->SetXY(122,127);
 $pdf->Cell(10,4,utf8_encode(mb_strtoupper(date("d",strtotime($t['licenciavalida'])),"utf8")),0,0,"C");
 
-$pdf->SetXY(143,123);
+$pdf->SetXY(143,127);
 $pdf->Cell(35,4,utf8_encode(mb_strtoupper(strftime("%B",strtotime($t['licenciavalida'])),"utf8")),0,0,"C");
 
-$pdf->SetXY(190,123);
+$pdf->SetXY(190,127);
 $pdf->Cell(15,4,utf8_encode(mb_strtoupper(date("Y",strtotime($t['licenciavalida'])),"utf8")),0,0,"C");
 
 
-$pdf->SetXY(24,133);
+$pdf->SetXY(24,137);
 $pdf->Cell(10,4,utf8_encode(mb_strtoupper(date("d",strtotime($t['fechaderegistro'])),"utf8")),0,0,"C");
 
-$pdf->SetXY(46,133);
+$pdf->SetXY(46,137);
 $pdf->Cell(35,4,utf8_encode(mb_strtoupper(strftime("%B",strtotime($t['fechaderegistro'])),"utf8")),0,0,"C");
 
-$pdf->SetXY(96,133);
+$pdf->SetXY(96,137);
 $pdf->Cell(15,4,utf8_encode(mb_strtoupper(date("Y",strtotime($t['fechaderegistro'])),"utf8")),0,0,"C");
 
 
-$pdf->SetXY(190,35);
+$pdf->SetXY(190,33);
 $pdf->Cell(15,4,utf8_encode(mb_strtoupper(date("Y",strtotime($t['fechaderegistro'])),"utf8")),0,0,"C");
 
 $foto="../../imagenes/fotografiapropetario/".$t['fotografiapropetario'];
 if(!empty($t['fotografiapropetario']) && file_exists($foto)){
-	$pdf->Image($foto,177,45,30,35);	
+	$pdf->Image($foto,178,43,32,37);	
 }
 
 $pdf->Output("reporte","I");

789751f418022a509ede3659a6e7deb53c02cda2
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-08-03 12:49:47 -0400

    Preparado Para Niveles de usuario

--- modalidad/registro/busqueda.php
+++ modalidad/registro/busqueda.php
@@ -8,6 +8,34 @@ if (!empty($_POST)) {
 	$modalidad=new modalidad;
 	$mod=$modalidad->mostrarTodo("nombre LIKE '%$nombre%'");
 	$titulo=array("nombre"=>"Nombre","descripcion"=>"Descripción","observacion"=>"Observación");
-	listadoTabla($titulo,$mod,1,"modificar.php","eliminar.php","");
+	$nivel=$_SESSION['nivel'];
+	
+	//$rep="ver.php";
+	$modi="modificar.php";
+	$eli="eliminar.php";
+	
+	switch($nivel){
+		case 1:{//$modi="";
+				//$eli="";
+				//$rep="";
+				}break;
+		case 2:{//$modi="";
+				//$eli="";
+				//$rep="";
+				}break;
+		case 3:{//$modi="";
+				//$eli="";
+				//$rep="";
+				}break;
+		case 4:{//$modi="";
+				//$eli="";
+				//$rep="";
+				}break;
+		case 5:{//$modi="";
+				//$eli="";
+				//$rep="";
+				}break;
+	}
+	listadoTabla($titulo,$mod,1,$modi,$eli,$rep);
 }
 ?>

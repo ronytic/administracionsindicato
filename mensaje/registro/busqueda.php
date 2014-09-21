65214efda66351839847221c66e52f111dd54a38
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-09-21 18:04:50 -0400

    Seguridad  Mensaje

--- mensaje/registro/busqueda.php
+++ mensaje/registro/busqueda.php
@@ -8,6 +8,32 @@ if (!empty($_POST)) {
 	$mensa=new mensa;
 	$mod=$mensa->mostrarTodo("mensaje LIKE '%$mensaje%'");
 	$titulo=array("mensaje"=>"Mensaje");
-	listadoTabla($titulo,$mod,1,"modificar.php","eliminar.php","");
+	//$rep="ver.php";
+	$modi="modificar.php";
+	$eli="eliminar.php";
+	
+	switch($nivel){
+		case 1:{//$modi="";//SuperAdmin
+				//$eli="";
+				//$rep="";
+				}break;
+		case 2:{//$modi="";//Dirección
+				//$eli="";
+				$rep="";
+				}break;
+		case 3:{//$modi="";//Unidad de Trafico
+				//$eli="";
+				$rep="";
+				}break;
+		case 4:{$modi="";//Tecnico
+				$eli="";
+				$rep="";
+				}break;
+		case 5:{$modi="";//Secretaria
+				$eli="";
+				$rep="";
+				}break;
+	}
+	listadoTabla($titulo,$mod,1,$modi,$eli,"");
 }
 ?>

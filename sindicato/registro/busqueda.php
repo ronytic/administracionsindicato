a555b0ef947d86de5f8f00144045386ff96c4582
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-09-21 18:05:08 -0400

    Seguridad Sindicato

--- sindicato/registro/busqueda.php
+++ sindicato/registro/busqueda.php
@@ -8,6 +8,33 @@ if (!empty($_POST)) {
 	$sindicato=new sindicato;
 	$sin=$sindicato->mostrarTodo("nombre LIKE '%$nombre%'","nombre");
 	$titulo=array("nombre"=>"Nombre","personeriajuridica"=>"Personería Jurídica","nombreresponsable"=>"Nombre Responsable","ciresponsable"=>"C.I. Responsable","telefono"=>"Teléfono","direccion"=>"Dirección");
-	listadoTabla($titulo,$sin,1,"modificar.php","eliminar.php","ver.php");
+	
+	$rep="ver.php";
+	$modi="modificar.php";
+	$eli="eliminar.php";
+	
+	switch($nivel){
+		case 1:{//$modi="";//SuperAdmin
+				//$eli="";
+				//$rep="";
+				}break;
+		case 2:{$modi="";//Dirección
+				$eli="";
+				//$rep="";
+				}break;
+		case 3:{$modi="";//Unidad de Trafico
+				$eli="";
+				//$rep="";
+				}break;
+		case 4:{//$modi="";//Tecnico
+				//$eli="";
+				//$rep="";
+				}break;
+		case 5:{$modi="";//Secretaria
+				$eli="";
+				//$rep="";
+				}break;
+	}
+	listadoTabla($titulo,$sin,1,$modi,$eli,$rep);
 }
 ?>

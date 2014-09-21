3b74bfd038e6ddaae11a8c4e84d70fdc1259e1fe
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2014-09-21 18:05:01 -0400

    Seguridad Modalidad

--- modalidad/servicios/busqueda.php
+++ modalidad/servicios/busqueda.php
@@ -8,6 +8,34 @@ if (!empty($_POST)) {
 	$servicio=new servicio;
 	$ser=$servicio->mostrarTodo("nombre LIKE '%$nombre%'");
 	$titulo=array("nombre"=>"Nombre","descripcion"=>"Descripción","observacion"=>"Observación");
-	listadoTabla($titulo,$ser,1,"modificar.php","eliminar.php","");
+	
+	//$rep="ver.php";
+	$modi="modificar.php";
+	$eli="eliminar.php";
+	
+	switch($nivel){
+		case 1:{//$modi="";//SuperAdmin
+				//$eli="";
+				//$rep="";
+				}break;
+		case 2:{$modi="";//Dirección
+				$eli="";
+				$rep="";
+				}break;
+		case 3:{$modi="";//Unidad de Trafico
+				$eli="";
+				$rep="";
+				}break;
+		case 4:{//$modi="";//Tecnico
+				//$eli="";
+				$rep="";
+				}break;
+		case 5:{$modi="";//Secretaria
+				$eli="";
+				$rep="";
+				}break;
+	}
+	
+	listadoTabla($titulo,$ser,1,$modi,$eli,$rep);
 }
 ?>
